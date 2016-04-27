563e5103ebd33cb93abb604c7fb10ed8d9ec94e4
Sergey Gernyak <sergeg1990@gmail.com>
2016-04-27 21:34:17 +0300

    Type fixes

--- src/browser/Store.js
+++ src/browser/Store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux'
-import reducers from './reducers'
+import reducers from './reducers/Index'
 import { routerReducer } from 'react-router-redux'
 
 // Add the reducer to your store on the `routing` key
