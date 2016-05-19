781f799c24587ee895761554561e9b28215987af
Sergey Gernyak <sergeg1990@gmail.com>
2016-05-19 07:54:22 +0300

    Fix problem with switching remarks lists

--- src/browser/reducers/Remarks.js
+++ src/browser/reducers/Remarks.js
@@ -1,12 +1,14 @@
 import { List } from 'immutable';
 
-import { LOAD_REMARKS, ADD_REMARK, CLEAN_REMARKS } from '../actions/Remarks';
+import { LOAD_REMARKS, ADD_REMARKS, ADD_REMARK, CLEAN_REMARKS } from '../actions/Remarks';
 
 const initialState = List([]);
 
 const Remarks = (remarks = initialState, action) => {
   switch (action.type) {
     case LOAD_REMARKS:
+      return List(action.remarks);
+    case ADD_REMARKS:
       return remarks.concat(List(action.remarks));
     case ADD_REMARK:
       return remarks.unshift(action.remark);
